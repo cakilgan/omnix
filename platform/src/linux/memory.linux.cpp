aea4b321fe61ceb7630a721415f44b2312b3677b
cakilgan <cakilganhan@gmail.com>
2026-03-08 12:06:17 +0300

    deleting memory api for rewrite.

--- platform/src/linux/memory.linux.cpp
+++ platform/src/linux/memory.linux.cpp
@@ -5,25 +5,25 @@
 #include <sys/mman.h>
 
 using namespace ox;
-
-result<memory> memory::alloc(bytes size) noexcept {
-    void* ptr = ::mmap(nullptr, size.ct,
-                   PROT_READ | PROT_WRITE,
-                   MAP_PRIVATE | MAP_ANONYMOUS,
-                   -1, 0);
-    if (ptr == MAP_FAILED) {
-        return err;
-    }else {
-        return memory{ptr,size};
-    }
-}
-
-void memory::free() noexcept {
-    if (_Data) {
-        ::munmap(_Data,_Size.ct);
-        _Data = nullptr;
-        _Size = bytes{0};
-        _owned = false;
-    }
-}
+//
+// result<memory> memory::alloc(bytes size) noexcept {
+//     void* ptr = ::mmap(nullptr, size.ct,
+//                    PROT_READ | PROT_WRITE,
+//                    MAP_PRIVATE | MAP_ANONYMOUS,
+//                    -1, 0);
+//     if (ptr == MAP_FAILED) {
+//         return err;
+//     }else {
+//         return memory{ptr,size};
+//     }
+// }
+//
+// void memory::free() noexcept {
+//     if (_Data) {
+//         ::munmap(_Data,_Size.ct);
+//         _Data = nullptr;
+//         _Size = bytes{0};
+//         _owned = false;
+//     }
+// }
 
