5c161ddca64503607f07e33ace8bf8061886fc58
cakilgan <cakilganhan@gmail.com>
2026-03-16 02:55:33 +0300

    added more utilities

--- platform/include/omnix/platform/util.h
+++ platform/include/omnix/platform/util.h
@@ -4,6 +4,8 @@
 
 #ifndef OMNIX_UTIL_H
 #define OMNIX_UTIL_H
+#include "result.h"
+
 namespace ox {
     //NOTE: exact copy of std::move but without any external includes
     template<typename _Tp>
@@ -22,7 +24,27 @@ namespace ox {
     { return static_cast<typename remove_reference<_Tp>::type&&>(__t); }
 
     OX_INLINE bool safe(vptr check) {
+        OX_ASSERT(check != nullptr);
         return check != nullptr;
+    }
+
+    template<typename T>
+    T unchecked(result<T> _result) {
+        OX_ASSERT(_result);
+        return ox::move(_result.value());
+    }
+
+    // for ignoring nodiscard
+    OX_INLINE void unchecked(const result_t _result) {
+        OX_ASSERT(_result==ok);
+    }
+
+
+    OX_FORCE_CONSTEXPR bool in_btw(const i64 x, const i64 start, const i64 end) {
+        return x >= start && x <= end;
+    }
+    OX_FORCE_CONSTEXPR bool contains(const i64 x, const i64 y, const i64 start, const i64 end) {
+        return x<=start && y>=end;
     }
 }
 #endif //OMNIX_UTIL_H
