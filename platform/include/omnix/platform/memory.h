9da0702f5027e74391ec38a91f7e1fab997589e5
cakilgan <cakilganhan@gmail.com>
2026-03-16 17:31:05 +0300

    memory api

--- platform/include/omnix/platform/memory.h
+++ platform/include/omnix/platform/memory.h
@@ -10,43 +10,189 @@
 #include "result.h"
 #include "byte.h"
 
-namespace ox {
-    //NOTE: not using ::ox::bytes cause of simplicity.
+//! do not include <assert.h>
+#include "../platform/assert.h"
 
-    OX_FORCE_CONSTEXPR usize align(usize size, usize alignment) {
+namespace ox {
+
+    // Align a size value to the given alignment.
+    OX_FORCE_CONSTEXPR usize align(const usize size, const usize alignment) {
         if (alignment == 0) return size;
-        usize remainder = size % alignment;
-        if (remainder == 0) return size;
+
+        const usize remainder = size % alignment;
+        if (remainder == 0)
+            return size;
+
         return size + (alignment - remainder);
     }
 
+    // Returns the next power of two greater than or equal to x.
     OX_FORCE_CONSTEXPR usize nextpow2(usize x) {
         if (x == 0) return 1;
+
         --x;
         x |= x >> 1;
         x |= x >> 2;
         x |= x >> 4;
         x |= x >> 8;
         x |= x >> 16;
-        if constexpr (sizeof(usize) > 4) x |= x >> 32;
+
+        if constexpr (sizeof(usize) > 4)
+            x |= x >> 32;
+
         return x + 1;
     }
 
+    // Checks whether x is a power of two.
     OX_FORCE_CONSTEXPR bool ispow2(usize x) {
         return x != 0 && (x & (x - 1)) == 0;
     }
 
-    OX_INLINE void* align_ptr(void* OX_RESTRICT ptr, const usize alignment) {
+    // Align a pointer to the given alignment.
+    OX_INLINE vptr align_ptr(cvptr ptr, const usize alignment) {
         auto p = reinterpret_cast<uptr>(ptr);
         p = (p + alignment - 1) & ~(alignment - 1);
-        return reinterpret_cast<void*>(p);
+        return reinterpret_cast<vptr>(p);
     }
 
-    OX_INLINE void* offset_ptr(void* OX_RESTRICT ptr, const usize offset) {
-        return reinterpret_cast<void*>(reinterpret_cast<uptr>(ptr) + offset);
+    // Returns ptr + offset with safe casting.
+    OX_INLINE vptr offset_ptr(cvptr ptr, const usize offset) {
+        return reinterpret_cast<vptr>(reinterpret_cast<uptr>(ptr) + offset);
     }
 
+
+    struct memory {
+
+        // Error codes
+        // range: -1321 ... -1328
+        static OX_CAUTO slice_overlap              = result_t{-1321};
+        static OX_CAUTO max_slice_overflow         = result_t{-1322};
+        static OX_CAUTO no_match_for_unslice       = result_t{-1323};
+        static OX_CAUTO already_released           = result_t{-1324};
+        static OX_CAUTO non_owner_release          = result_t{-1325};
+        static OX_CAUTO allocation_error           = result_t{-1326};
+        static OX_CAUTO cannot_find_suitable_memory= result_t{-1327};
+        static OX_CAUTO null_parameter             = result_t{-1328};
+
+
+        // Maximum number of slices a memory object can track.
+        static OX_FORCE_CONSTEXPR u8 _max_slices_ = 16;
+
+        // Stores information about a single slice.
+        struct slice_record {
+            loc   start;
+            bytes size;
+        };
+
+        // Slice record stack
+        slice_record _slices_[_max_slices_]{};
+
+        // Current slice count
+        u8 _slice_count_ = 0;
+
+
+        /*
+         * When a new slice is created using memory::slice(),
+         * the child's _slice_start is set to the "start" parameter.
+         *
+         * This value is used internally for bounds checking
+         * and general slice calculations inside memory functions.
+         */
+        loc _slice_start = 0;
+
+
+        // Memory types:
+        //
+        // parent : created with memory::allocate()
+        // child  : created from slicing another memory
+        // invalid: memory has been released or is unusable
+        enum type {
+            parent,
+            child,
+            invalid
+        };
+
+
+        // Default constructor
+        memory() : _Data(nullptr), _Size(0), _Type(invalid) {}
+
+
+        // Move constructor
+        // Memory objects can only be transferred via move.
+        memory(memory&& o) noexcept {
+            _Data = o._Data;
+            _Size = o._Size;
+            _Type = o._Type;
+
+            o._Data = nullptr;
+            o._Size = 0;
+            o._Type = invalid;
+        }
+
+
+        // Disable copy operations
+        memory(const memory&) = delete;
+        memory& operator=(const memory&) = delete;
+        memory& operator=(memory) = delete;
+
+
+        // Allocate memory of given size
+        static result<memory> allocate(bytes size);
+
+        // Release allocated memory
+        static result_t release(memory& o);
+
+        // Create a slice from the memory
+        // range: [start, start + size)
+        static result<memory> slice(memory& o, loc start, bytes size, loc ignore_start = -1);
+
+        // Remove a slice from its parent
+        static result_t unslice(memory& parent, memory& child, bool set_zero = false);
+
+        // Grow a slice inside its parent memory
+        static result_t grow(memory& parent, memory& child, bytes size);
+
+        /*
+         * Finds the first suitable location for a slice of the given size.
+         *
+         * Typical usage:
+         *     slice(big_memory, find(big_memory, 1024), 1024);
+         */
+        static loc find(memory& mem, bytes size, loc ignore_start = -1);
+
+
+        ~memory() {
+            // If this object owns the memory, release it automatically.
+            if (is_owner())
+                OX_VERIFY(release(*this) == ok);
+        }
+
+
+        // Getters
+
+        vptr data() const noexcept {
+            return _Data;
+        }
+
+        bytes size() const noexcept {
+            return _Size;
+        }
+
+        bool is_owner() const noexcept {
+            return _Type == parent;
+        }
+
+    private:
+
+        vptr  _Data;
+        bytes _Size;
+        type  _Type;
+
+        // Internal constructor
+        memory(vptr data, const bytes size, const type type)
+            : _Data(data), _Size(size), _Type(type) {}
+    };
 
 } // namespace ox
 
-#endif //OMNIX_MEMORY_H
+#endif // OMNIX_MEMORY_H
--- platform/src/linux/memory.linux.cpp
+++ platform/src/linux/memory.linux.cpp
@@ -1,29 +1,136 @@
 //
 // Created by cakilgan on 3/7/26.
 //
+#include <cstring>
 #include <omnix/platform/memory.h>
 #include <sys/mman.h>
+#include <omnix/platform/util.h>
 
 using namespace ox;
-//
-// result<memory> memory::alloc(bytes size) noexcept {
-//     void* ptr = ::mmap(nullptr, size.ct,
-//                    PROT_READ | PROT_WRITE,
-//                    MAP_PRIVATE | MAP_ANONYMOUS,
-//                    -1, 0);
-//     if (ptr == MAP_FAILED) {
-//         return err;
-//     }else {
-//         return memory{ptr,size};
-//     }
-// }
-//
-// void memory::free() noexcept {
-//     if (_Data) {
-//         ::munmap(_Data,_Size.ct);
-//         _Data = nullptr;
-//         _Size = bytes{0};
-//         _owned = false;
-//     }
-// }
 
+
+result<memory> memory::allocate(bytes size) {
+    void* ptr = ::mmap(nullptr, size,
+           PROT_READ | PROT_WRITE,
+           MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE,
+           -1, 0);
+    if (ptr == MAP_FAILED) {
+        return allocation_error;
+    }
+    return memory{ptr,size,parent};
+}
+
+result_t       memory::release (memory& o) {
+    OX_ASSERT(o.is_owner());
+    if (!o.is_owner()) return non_owner_release;
+    if (o._Data) {
+        ::munmap(o._Data,o._Size);
+        o._Data = nullptr;
+        o._Size = 0;
+        o._Type = type::invalid;
+        return ok;
+    }
+    return already_released;
+}
+
+result<memory> memory::slice(memory &o, const loc start, const bytes size, loc ignore_start) {
+    OX_ASSERT(start + size.ct <= o._Size);
+    if (o._slice_count_>=_max_slices_) {
+        OX_FAIL("max_slice_overflow");
+        return max_slice_overflow;
+    }
+    for (u8 i = 0; i < o._slice_count_; i++) {
+        const auto slice = o._slices_[i];
+
+        // i am a moron.
+        if (in_btw(start,slice.start,slice.size)||
+            in_btw(start+size,slice.start,slice.size)||
+            contains(start,start+size,slice.start,slice.size)||
+            contains(slice.start,slice.size,start,size)) {
+            if (slice.start == ignore_start) continue;
+            OX_FAIL("memory slice overlap.");
+            return slice_overlap;
+        }
+    }
+    memory mem{(offset_ptr(o._Data,start)),size,child};
+    mem._slice_start = start;
+    u8 pos = o._slice_count_;
+    for (u8 i = 0; i < o._slice_count_; i++) {
+        if (o._slices_[i].start > start) {
+            pos = i;
+            break;
+        }
+    }
+    for (u8 i = o._slice_count_; i > pos; i--)
+        o._slices_[i] = o._slices_[i-1];
+    o._slices_[pos] = {start, size};
+    o._slice_count_++;
+    return mem;
+}
+
+result_t memory::unslice(memory &owner, memory &child,bool set_zero) {
+    bool found = false;
+    for (u8 i = 0; i < owner._slice_count_; i++) {
+        if (owner._slices_[i].start == child._slice_start) {
+            for (u8 x = i; x < owner._slice_count_ - 1; x++) {
+                owner._slices_[x] = owner._slices_[x+1];
+            }
+            owner._slice_count_--;
+            found = true;
+        }
+    }
+    if (!found) {
+        OX_FAIL("child not found in owner.");
+        return no_match_for_unslice;
+    }
+
+    if (set_zero) {
+        memset(child.data(),0,child.size());
+    }
+    child._Data = nullptr;
+    child._Size = 0;
+    child._Type = type::invalid;
+    return ok;
+}
+
+loc memory::find(memory &parent, bytes size, loc ignore_start) {
+    loc cursor = 0;
+    bool _ignore = ignore_start!=-1;
+    for (u8 i = 0; i < parent._slice_count_; i++) {
+        const auto slice = parent._slices_[i];
+        if (_ignore && slice.start == ignore_start) continue;
+        if (slice.start-cursor >= size) return cursor;
+        cursor = slice.start+slice.size;
+    }
+    if (parent._Size - cursor >= size) return cursor;
+    return -1;
+}
+
+
+result_t memory::grow(memory &parent, memory &child, bytes size) {
+    auto loc = find(parent,size,child._slice_start);
+    if (loc==-1) return cannot_find_suitable_memory;
+
+    auto mhndl = slice(parent,loc,size,child._slice_start);
+    if (!mhndl) return mhndl.err();
+
+    auto new_memory = ox::move(mhndl.value());
+
+    void* old_data = child._Data;
+    auto old_size = child._Size;
+
+    memmove(new_memory.data(), old_data, old_size);
+
+    unslice(parent,child,false);
+
+    child._Data = new_memory._Data;
+    child._Size = new_memory._Size;
+    child._Type = new_memory._Type;
+    child._slice_start = new_memory._slice_start;
+
+    new_memory._Data = nullptr;
+    new_memory._Type = type::invalid;
+    new_memory._Size = 0;
+
+    return ok;
+}
