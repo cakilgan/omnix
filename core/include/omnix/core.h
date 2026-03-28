cde73e6a2c9884f0a705ba94a2bcd8c8bc6b903a
cakilgan <cakilganhan@gmail.com>
2026-03-29 01:34:54 +0300

    init main core header.

---
+++ core/include/omnix/core.h
@@ -0,0 +1,12 @@
+//
+// Created by cakilgan on 3/20/26.
+//
+
+#ifndef OMNIX_CORE_H
+#define OMNIX_CORE_H
+#include "core/hash.h"
+#include "core/hashmap.h"
+#include "core/string.h"
+#include "core/strview.h"
+#include "core/vector.h"
+#endif //OMNIX_CORE_H
