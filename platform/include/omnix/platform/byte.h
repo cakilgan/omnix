0701818081b05e387fc04f26d844bfe4763b7756
cakilgan <cakilganhan@gmail.com>
2026-03-29 01:31:29 +0300

    better location and bytes(size) support

--- platform/include/omnix/platform/byte.h
+++ platform/include/omnix/platform/byte.h
@@ -1,133 +1,105 @@
 //
-// Created by cakilgan on 3/4/26.
+// Created by cakilgan on 3/18/26.
 //
 
 #ifndef OMNIX_BYTE_H
 #define OMNIX_BYTE_H
 #pragma once
-
-#include "defines.h"
-#include "types.h"
-
 #ifndef OX_BYTE_NO_STD_HELPERS
 #include <bitset>
 #include <iostream>
 #include <string>
 #endif
 
+#include "../platform/assert.h"
+#include "defines.h"
+#include "types.h"
+#include "constants.h"
+#include "loc.h"
+
 namespace ox {
-		struct byte{
-		public:
-			struct _Bits {
-				_Bits() = default;
-#if OX_IS(ENDIAN,BIG)
-				u8 b7 : 1, b6 : 1, b5 : 1, b4 : 1, b3 : 1, b2 : 1, b1 : 1, b0 : 1;
-#else
-				u8 b0 : 1, b1 : 1, b2 : 1, b3 : 1, b4 : 1, b5 : 1, b6 : 1, b7 : 1;
-#endif
-			};
-		private:
-			union {
-				u8 value;
-				_Bits Bits;
-			};
-		public:
 
-			OX_FORCE_CONSTEXPR OX_FORCE_INLINE byte(u8 _Set = 0):value(_Set){}
+    struct byte {
+        u8 value;
+    };
 
-			OX_FORCE_INLINE void set(i32 pos) { value |= (1 << pos); };
-			OX_FORCE_INLINE void clear(i32 pos) { value &= ~(1 << pos); };
-			OX_FORCE_INLINE void flip(i32 pos) { value ^= (1 << pos); };
-			OX_FORCE_CONSTEXPR OX_FORCE_INLINE bool test(i32 pos) const { return (value >> pos) & 1; };
-
-			OX_FORCE_CONSTEXPR OX_FORCE_INLINE byte operator|(byte other) const { return {static_cast<u8>(value | other.value)}; }
-			OX_FORCE_CONSTEXPR OX_FORCE_INLINE byte operator&(byte other) const { return {static_cast<u8>(value & other.value)}; }
-			OX_FORCE_CONSTEXPR OX_FORCE_INLINE byte operator^(byte other) const { return {static_cast<u8>(value ^ other.value)}; }
-			OX_FORCE_CONSTEXPR OX_FORCE_INLINE byte operator<<(i32 shift) const { return {static_cast<u8>(value << shift)}; }
-			OX_FORCE_CONSTEXPR OX_FORCE_INLINE byte operator>>(i32 shift) const { return {static_cast<u8>(value >> shift)}; }
-
-			OX_FORCE_INLINE byte& operator|=(byte other) { value |= other.value; return *this; };
-			OX_FORCE_INLINE byte& operator&=(byte other) { value &= other.value; return *this; };
-			OX_FORCE_INLINE byte& operator^=(byte other) { value ^= other.value; return *this; };
-			OX_FORCE_INLINE byte& operator<<=(i32 shift) { value <<= shift; return *this; };
-			OX_FORCE_INLINE byte& operator>>=(i32 shift) { value >>= shift; return *this; };
-
-			OX_FORCE_CONSTEXPR OX_FORCE_INLINE byte operator~() const { return byte(static_cast<u8>(~value)); };
-			OX_FORCE_CONSTEXPR OX_FORCE_INLINE bool operator==(byte other) const { return value == other.value; };
-			OX_FORCE_CONSTEXPR OX_FORCE_INLINE bool operator!=(byte other) const { return value != other.value; };
-
-			OX_FORCE_CONSTEXPR OX_FORCE_INLINE u8 to_u8() const { return value; }
-
-			OX_FORCE_INLINE _Bits to_bits() const { return Bits; }
-			OX_FORCE_INLINE _Bits& bits() { return Bits; };
+    OX_FORCE_INLINE void bit_set(byte& b, i32 pos)    { b.value |= (1 << pos); }
+    OX_FORCE_INLINE void bit_clear(byte& b, i32 pos)  { b.value &= ~(1 << pos); }
+    OX_FORCE_INLINE void bit_flip(byte& b, i32 pos)   { b.value ^= (1 << pos); }
+    OX_FORCE_CONSTEXPR OX_FORCE_INLINE bool bit_test(const byte& b, i32 pos) { return (b.value >> pos) & 1; }
 
 #ifndef OX_BYTE_NO_STD_HELPERS
-			OX_FORCE_INLINE std::string to_binary() const {
-				return std::bitset<8>(value).to_string();
-			}
-			OX_FORCE_INLINE friend std::ostream& operator<<(std::ostream& os, const byte& b) {
-				os << "0x" << std::hex << static_cast<int>(b.value) << std::dec << " (" << b.to_binary() << ")";
-				return os;
-			}
+    OX_FORCE_INLINE std::string to_binary(const byte& x){
+        return std::bitset<8>(x.value).to_string();
+    }
+    OX_FORCE_INLINE std::ostream& operator<<(std::ostream& os, const byte& b) {
+        os << "0x" << std::hex << static_cast<int>(b.value) << std::dec << " (" << to_binary(b) << ")";
+        return os;
+    }
 #endif
-		};
 
-		OX_STATIC_ASSERT(sizeof(::ox::byte) == 1, "size of omnix#byte must be exactly 1 byte.");
-		OX_USING(bytes_t, i64);
+    OX_STATIC_ASSERT(sizeof(byte) == 1, "ox::byte must be exactly 1 byte");
 
-		struct bytes {
-		    bytes_t ct;
+    OX_USING(bytes_t, u64);
 
-		    OX_FORCE_CONSTEXPR bytes() : ct(0) {}
-		    OX_FORCE_CONSTEXPR bytes(bytes_t b) : ct(b) {}
+    struct bytes {
+        bytes_t ct;
 
-		    OX_FORCE_CONSTEXPR operator bytes_t() const { return ct; }
-		    OX_FORCE_CONSTEXPR operator bytes_t&()       { return ct; }
+        OX_FORCE_CONSTEXPR bytes() : ct(0) {}
+        explicit OX_FORCE_CONSTEXPR bytes(bytes_t b) : ct(b) {}
+        explicit OX_FORCE_CONSTEXPR bytes(loc b) : ct(b.value) {}
 
-		    OX_FORCE_CONSTEXPR bool operator==(const bytes& rhs) const { return ct == rhs.ct; }
-		    OX_FORCE_CONSTEXPR bool operator!=(const bytes& rhs) const { return ct != rhs.ct; }
-		    OX_FORCE_CONSTEXPR bool operator< (const bytes& rhs) const { return ct <  rhs.ct; }
-		    OX_FORCE_CONSTEXPR bool operator<=(const bytes& rhs) const { return ct <= rhs.ct; }
-		    OX_FORCE_CONSTEXPR bool operator> (const bytes& rhs) const { return ct >  rhs.ct; }
-		    OX_FORCE_CONSTEXPR bool operator>=(const bytes& rhs) const { return ct >= rhs.ct; }
+        OX_FORCE_CONSTEXPR bool operator==(const bytes& rhs) const { return ct == rhs.ct; }
+        OX_FORCE_CONSTEXPR bool operator!=(const bytes& rhs) const { return ct != rhs.ct; }
+        OX_FORCE_CONSTEXPR bool operator< (const bytes& rhs) const { return ct <  rhs.ct; }
+        OX_FORCE_CONSTEXPR bool operator<=(const bytes& rhs) const { return ct <= rhs.ct; }
+        OX_FORCE_CONSTEXPR bool operator> (const bytes& rhs) const { return ct >  rhs.ct; }
+        OX_FORCE_CONSTEXPR bool operator>=(const bytes& rhs) const { return ct >= rhs.ct; }
 
-		    OX_FORCE_CONSTEXPR bytes& operator+=(const bytes& rhs) { ct += rhs.ct; return *this; }
-		    OX_FORCE_CONSTEXPR bytes& operator-=(const bytes& rhs) { ct -= rhs.ct; return *this; }
-		    OX_FORCE_CONSTEXPR bytes& operator*=(bytes_t scalar)   { ct *= scalar;  return *this; }
-		    OX_FORCE_CONSTEXPR bytes& operator/=(bytes_t scalar)   { ct /= scalar;  return *this; }
-		};
+        OX_FORCE_CONSTEXPR bytes& operator+=(const bytes& rhs) { ct += rhs.ct; return *this; }
+        OX_FORCE_CONSTEXPR bytes& operator-=(const bytes& rhs) { ct -= rhs.ct; return *this; }
+        OX_FORCE_CONSTEXPR bytes& operator*=(bytes_t scalar) { ct *= scalar; return *this; }
+        OX_FORCE_CONSTEXPR bytes& operator/=(bytes_t scalar) { ct /= scalar; return *this; }
 
-		OX_FORCE_CONSTEXPR OX_INLINE bytes   gigabytes(const bytes_t gb)    { return gb * 1024*1024*1024LL; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes_t gigabytes(const bytes& gb)		{ return gb.ct / (1024LL*1024*1024); }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes   megabytes(const bytes_t mb)    { return mb * 1024LL*1024; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes_t megabytes(const bytes& mb)		{ return mb.ct / (1024LL*1024); }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes   kilobytes(const bytes_t kb)    { return kb * 1024LL; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes_t kilobytes(const bytes& kb)		{ return kb.ct / 1024LL; }
+        OX_FORCE_CONSTEXPR bytes operator-(const bytes& rhs) const { return bytes(ct - rhs.ct);}
+        OX_FORCE_CONSTEXPR bytes operator+(const bytes& rhs) const { return bytes(ct + rhs.ct);}
+        OX_FORCE_CONSTEXPR bytes operator%(const bytes& rhs) const { return bytes(ct % rhs.ct);}
+    };
 
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator-(const bytes& a, const bytes& b) { return a.ct - b.ct; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator+(const bytes& a, const bytes& b) { return a.ct + b.ct; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator/(const bytes& a, const bytes& b) { return a.ct / b.ct; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator*(const bytes& a, const bytes& b) { return a.ct * b.ct; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator%(const bytes& a, const bytes& b) { return a.ct % b.ct; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator&(const bytes& a, const bytes& b) { return a.ct & b.ct; }
+    OX_FORCE_CONSTEXPR OX_INLINE bytes gigabytes(const bytes_t gb) { return bytes(gb * 1024LL * 1024LL * 1024LL); }
+    OX_FORCE_CONSTEXPR OX_INLINE bytes_t gigabytes(const bytes& gb) { return gb.ct / (1024LL*1024*1024); }
 
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator-(const loc&  a, const bytes& b) { return a - b.ct; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator+(const loc&  a, const bytes& b) { return a + b.ct; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator/(const loc&  a, const bytes& b) { return a / b.ct; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator*(const loc&  a, const bytes& b) { return a * b.ct; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator%(const loc&  a, const bytes& b) { return a % b.ct; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator&(const loc&  a, const bytes& b) { return a & b.ct; }
+    OX_FORCE_CONSTEXPR OX_INLINE bytes megabytes(const bytes_t mb) { return bytes(mb * 1024LL * 1024LL); }
+    OX_FORCE_CONSTEXPR OX_INLINE bytes_t megabytes(const bytes& mb) { return mb.ct / (1024LL*1024); }
 
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator-(const bytes& a, const loc& b) { return a.ct - b; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator+(const bytes& a, const loc& b) { return a.ct + b; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator/(const bytes& a, const loc& b) { return a.ct / b; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator*(const bytes& a, const loc& b) { return a.ct * b; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator%(const bytes& a, const loc& b) { return a.ct % b; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator&(const bytes& a, const loc& b) { return a.ct & b; }
+    OX_FORCE_CONSTEXPR OX_INLINE bytes kilobytes(const bytes_t kb) { return bytes(kb * 1024LL); }
+    OX_FORCE_CONSTEXPR OX_INLINE bytes_t kilobytes(const bytes& kb) { return kb.ct / 1024LL; }
 
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator*(const bytes& a, const i64 scalar) { return a.ct * scalar; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator/(const bytes& a, const i64 scalar) { return a.ct / scalar; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator*(const i64 scalar, const bytes& a) { return a.ct * scalar; }
-		OX_FORCE_CONSTEXPR OX_INLINE bytes operator/(const i64 scalar, const bytes& a) { return scalar / a.ct; }
-}
-#endif //OMNIX_BYTE_H
+    OX_FORCE_CONSTEXPR OX_INLINE bytes operator*(const bytes& a, const u64 scalar) { return bytes(a.ct * scalar); }
+    OX_FORCE_CONSTEXPR OX_INLINE bytes operator*(const u64 scalar, const bytes& a) { return bytes(a.ct * scalar); }
+    OX_FORCE_CONSTEXPR OX_INLINE bytes operator/(const bytes& a, const u64 scalar) { return bytes(a.ct / scalar); }
+
+    OX_FORCE_CONSTEXPR OX_INLINE byte* operator+(byte* ptr, const bytes& add) { return ptr + add.ct; }
+    OX_FORCE_CONSTEXPR OX_INLINE byte* operator-(byte* ptr, const bytes& add) { return ptr - add.ct; }
+    OX_FORCE_CONSTEXPR OX_INLINE const byte* operator+(const byte* ptr, const bytes& add) { return ptr + add.ct; }
+    OX_FORCE_CONSTEXPR OX_INLINE const byte* operator-(const byte* ptr, const bytes& add) { return ptr - add.ct; }
+
+    OX_FORCE_CONSTEXPR OX_INLINE loc operator+(loc l, const bytes& b) {
+        OX_ASSERT(b.ct <= (u64_max - l.value));
+        return loc{l.value + b.ct};
+    }
+    OX_FORCE_CONSTEXPR OX_INLINE loc operator-(loc l, const bytes& b) {
+        OX_ASSERT(l.value >= b.ct);
+        return loc{l.value - b.ct};
+    }
+    OX_FORCE_CONSTEXPR loc& operator+=(loc& l, const bytes& b) {
+        l = l + b;
+        return l;
+    }
+    OX_FORCE_CONSTEXPR loc& operator-=(loc& l, const bytes& b) {
+        l = l - b;
+        return l;
+    }
+} // namespace ox
+
+#endif // OMNIX_BYTE_H
---
+++ platform/include/omnix/platform/loc.h
@@ -0,0 +1,69 @@
+//
+// Created by cakilgan on 3/19/26.
+//
+
+#ifndef OMNIX_LOC_H
+#define OMNIX_LOC_H
+
+#include "types.h"
+#include "constants.h"
+
+namespace ox{
+    namespace types {
+        struct loc {
+            u64 value;
+            explicit OX_FORCE_CONSTEXPR loc(const u64 v = 0x0) : value(v) {}
+            OX_FORCE_CONSTEXPR OX_INLINE loc &operator++() {
+                value++;
+                return *this;
+            }
+            OX_FORCE_CONSTEXPR OX_INLINE loc operator++(int _dummy) {
+                auto old_v = value;
+                value++;
+                return loc{old_v};
+            }
+
+        };
+        OX_FORCE_CONSTEXPR bool operator==(loc a, loc b) { return a.value == b.value; }
+        OX_FORCE_CONSTEXPR bool operator!=(loc a, loc b) { return a.value != b.value; }
+        OX_FORCE_CONSTEXPR bool operator< (loc a, loc b) { return a.value <  b.value; }
+        OX_FORCE_CONSTEXPR bool operator<=(loc a, loc b) { return a.value <= b.value; }
+        OX_FORCE_CONSTEXPR bool operator> (loc a, loc b) { return a.value >  b.value; }
+        OX_FORCE_CONSTEXPR bool operator>=(loc a, loc b) { return a.value >= b.value; }
+        OX_FORCE_CONSTEXPR OX_INLINE loc operator+(loc a, loc b) {
+            return loc{ a.value + b.value };
+        }
+
+        struct sloc {
+            i64 value;
+            explicit OX_FORCE_CONSTEXPR sloc(const i64 v = 0x0) : value(v){}
+        };
+        OX_FORCE_CONSTEXPR bool operator==(sloc a, sloc b) { return a.value == b.value; }
+        OX_FORCE_CONSTEXPR bool operator!=(sloc a, sloc b) { return a.value != b.value; }
+        OX_FORCE_CONSTEXPR bool operator< (sloc a, sloc b) { return a.value <  b.value; }
+        OX_FORCE_CONSTEXPR bool operator<=(sloc a, sloc b) { return a.value <= b.value; }
+        OX_FORCE_CONSTEXPR bool operator> (sloc a, sloc b) { return a.value >  b.value; }
+        OX_FORCE_CONSTEXPR bool operator>=(sloc a, sloc b) { return a.value >= b.value; }
+        OX_FORCE_CONSTEXPR OX_INLINE sloc operator+(sloc a, sloc b) {
+            return sloc{ a.value + b.value };
+        }
+        OX_FORCE_CONSTEXPR OX_INLINE sloc operator-(sloc a, sloc b) {
+            return sloc{ a.value - b.value };
+        }
+    }
+
+    namespace constants {
+        OX_FORCE_CONSTEXPR loc  loczero   {0x0};
+        OX_FORCE_CONSTEXPR loc  locinvalid   {u64_max};
+        OX_FORCE_CONSTEXPR sloc sloczero {0x0};
+        OX_FORCE_CONSTEXPR sloc slocinvalid {i64_max};
+    }
+
+    using ox::constants::loczero;
+    using ox::constants::locinvalid;
+    using ox::constants::sloczero;
+    using ox::constants::slocinvalid;
+}
+
+
+#endif //OMNIX_LOC_H
