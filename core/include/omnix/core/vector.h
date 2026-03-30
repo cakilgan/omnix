2b8e7fb6ebad1f3506b2dacbd22be5cf21688d69
cakilgan <cakilganhan@gmail.com>
2026-03-30 18:25:42 +0300

    allocator interface , new constructors, back function, move copy assignment.

--- core/include/omnix/core/vector.h
+++ core/include/omnix/core/vector.h
@@ -10,9 +10,14 @@
 #include <omnix/platform/result.h>
 #include <omnix/platform/util.h>
 
+#include "engine_memory_internal.h"
+#include "allocators/allocator.h"
+
+#include <initializer_list>
+
 namespace ox {
     namespace containers {
-        template <typename T, typename Allocator>
+        template <typename T, typename Allocator = allocator>
         struct vector {
         private:
             Allocator* _allocator{nullptr};
@@ -25,15 +30,27 @@ namespace ox {
             }
 
             result_t _u_grow() {
-                usize new_capacity = (_capacity == 0) ? 4 : _capacity * 2;
-                T* new_data = static_cast<T*>(_allocator->alloc(_u_convert(new_capacity)));
+                const usize new_capacity = (_capacity == 0) ? 4 : _capacity * 2;
+
+                T* new_data = static_cast<T*>(
+                    _allocator->grow(
+                        _data,
+                        _u_convert(_capacity),
+                        _u_convert(new_capacity)
+                    )
+                );
+
+                OX_ASSERT(new_data);
+                if (!new_data) return memory::err::allocation_error;
+
+                for (usize i = 0; i < _size; ++i) {
+                    new (new_data + i) T(ox::move(_data[i]));
+                }
 
                 if (_data) {
                     for (usize i = 0; i < _size; ++i) {
-                        new (new_data + i) T(ox::move(_data[i]));
                         _data[i].~T();
                     }
-                    _allocator->free(_data);
                 }
 
                 _data = new_data;
@@ -50,10 +67,34 @@ namespace ox {
                 _data = static_cast<T*>(_allocator->alloc(_u_convert(_capacity)));
                 OX_ASSERT(_data != nullptr);
             }
-            explicit vector() = default;
+            explicit vector(engine::memory::type memory_type = engine::memory::GENERAL, usize reserve = 4)
+                :vector(ox::safe(engine::memory::get_allocator<T>(memory_type)),reserve){
+            }
+            explicit vector(std::initializer_list<T> list,engine::memory::type memory_type = engine::memory::GENERAL, usize reserve = 4)
+                :vector(memory_type,reserve){
+                for (const auto& elem : list) {
+                    new (_data + _size++) T(elem);
+                }
+            }
 
             vector(const vector&)            = delete;
             vector& operator=(const vector&) = delete;
+
+            vector& operator=(vector&& other) noexcept {
+                if (this != &other) {
+                    this->~vector();
+
+                    _data = other._data;
+                    _size = other._size;
+                    _capacity = other._capacity;
+                    _allocator = other._allocator;
+
+                    other._data = nullptr;
+                    other._size = 0;
+                    other._capacity = 0;
+                }
+                return *this;
+            }
 
             vector(vector&& o) noexcept
                 : _allocator(o._allocator), _data(o._data),
@@ -77,11 +118,18 @@ namespace ox {
             template<typename... Args>
             T& emplace_back(Args&&... args) {
                 if (_u_should_grow()) OX_VERIFY(_u_grow() == ok);
-                new (_data + _size++) T(ox::forward<Args>(args)...);
-                return *(_data+_size);
+                new (_data + _size) T(ox::forward<Args>(args)...);
+                return _data[_size++];
             }
             void push_back(const T& x) { emplace_back(x); }
             void push_back(T&& x)      { emplace_back(ox::move(x)); }
+
+            T& back() {
+                return _data[_size-1];
+            }
+            T& back() const {
+                return _data[_size-1];
+            }
 
             void  clear() {
                 for (usize i = 0; i < _size; ++i) {
