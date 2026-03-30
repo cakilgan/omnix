c4c2fe88c70af4014fa45cf34a1b02dc0fa35f88
cakilgan <cakilganhan@gmail.com>
2026-03-31 00:37:43 +0300

    adding kernel memory to global access

--- core/src/core.cpp
+++ core/src/core.cpp
@@ -2,6 +2,7 @@
 // Created by cakilgan on 3/16/26.
 //
 
+#include "omnix/core/allocators/freelist_allocator.h"
 #include "omnix/core/engine_memory_internal.h"
 
 namespace engine::memory {
@@ -9,5 +10,8 @@ namespace engine::memory {
         ox::freelist_allocator* small  = nullptr;
         ox::freelist_allocator* medium = nullptr;
         ox::freelist_allocator* big  = nullptr;
+    }
+    namespace kernel {
+        ox::freelist_allocator* single = nullptr;
     }
 }
