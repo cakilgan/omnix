8d0e5ae600c7659ebb90a991be823f98c1cc860a
cakilgan <cakilganhan@gmail.com>
2026-03-30 18:31:05 +0300

    init engine memory api

---
+++ core/include/omnix/core/engine_memory_internal.h
@@ -0,0 +1,37 @@
+//
+// Created by cakilgan on 3/30/26.
+//
+
+#ifndef OMNIX_ENGINE_MEMORY_INTERNAL_H
+#define OMNIX_ENGINE_MEMORY_INTERNAL_H
+#include "allocators/freelist_allocator.h"
+#include "omnix/platform/types.h"
+#include "omnix/platform/types.h"
+
+namespace engine::memory {
+    enum type {
+        GENERAL,
+        FRAME,
+        RESOURCE
+    };
+    namespace general {
+        extern ox::freelist_allocator* small;
+        extern ox::freelist_allocator* medium;
+        extern ox::freelist_allocator* big;
+    }
+
+    template<typename T>
+    ox::allocator* get_allocator(type memory_type) {
+        constexpr auto size = ox::size_of<T>();
+        if (memory_type == GENERAL) {
+            if constexpr (size < ox::bytes(32))  return general::small;
+            if constexpr (size > ox::bytes(256)) return general::big;
+            return general::medium;
+        }
+        return nullptr;
+    }
+
+
+
+}
+#endif //OMNIX_ENGINE_MEMORY_INTERNAL_H
--- core/src/core.cpp
+++ core/src/core.cpp
@@ -1,3 +1,13 @@
 //
 // Created by cakilgan on 3/16/26.
 //
+
+#include "omnix/core/engine_memory_internal.h"
+
+namespace engine::memory {
+    namespace general {
+        ox::freelist_allocator* small  = nullptr;
+        ox::freelist_allocator* medium = nullptr;
+        ox::freelist_allocator* big  = nullptr;
+    }
+}
