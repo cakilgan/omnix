2c9217bec19937557d827093d99c6274e31e0d56
cakilgan <cakilganhan@gmail.com>
2026-03-30 18:24:01 +0300

    making string_view template and adding new default strview templates

--- core/include/omnix/core/strview.h
+++ core/include/omnix/core/strview.h
@@ -7,19 +7,24 @@
 #include <omnix/platform/types.h>
 #include <omnix/platform/util.h>
 namespace ox {
-    struct strview {
-        cstr data {nullptr};
+    template<typename T>
+    struct string_view {
+        const T* data {nullptr};
         usize len {0};
+
         template<usize N>
-        constexpr strview(const char (&arr)[N])
+        constexpr string_view(const T (&arr)[N])
         : data(arr), len(N-1) {}
 
-        constexpr strview(const char* arr)
+        constexpr string_view(const T* arr)
         : data(arr), len(strlen(arr)) {}
 
-        constexpr strview():data(nullptr),len(0){}
+        constexpr string_view(const T* arr,usize len)
+        : data(arr), len(len) {}
 
-        constexpr bool operator==(const strview& other) const {
+        constexpr string_view():data(nullptr),len(0){}
+
+        constexpr bool operator==(const string_view& other) const {
             if (len != other.len) return false;
             for (usize i = 0; i < len; ++i) {
                 if (data[i] != other.data[i]) return false;
@@ -27,5 +32,8 @@ namespace ox {
             return true;
         }
     };
+    using strview   = string_view<char>;
+    using str16view = string_view<char16_t>;
+    using str32view = string_view<char32_t>;
 }
 #endif //OMNIX_STRVIEW_H
