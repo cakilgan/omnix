67eb3448736378a09aa2772e5a82dd10e7eb4571
cakilgan <cakilganhan@gmail.com>
2026-04-05 16:02:22 +0300

    new game (final) layer

---
+++ game/setup.cpp
@@ -0,0 +1,14 @@
+//
+// Created by cakilgan on 4/2/26.
+//
+
+#include <iostream>
+#include <omnix/omnix.h>
+#include <omnix/runtime/setup.h>
+
+#include "omnix/kernel/context.h"
+
+
+ox::result_t engine::setup(engine::setup_context* cx) {
+    return ox::ok;
+}
---
+++ runtime/include/omnix/runtime/setup.h
@@ -0,0 +1,15 @@
+//
+// Created by cakilgan on 4/2/26.
+//
+
+#ifndef OMNIX_SETUP_H
+#define OMNIX_SETUP_H
+#include <omnix/platform/result.h>
+#include "omnix/kernel/eventbus.h"
+
+namespace engine {
+    struct setup_context {
+    };
+    ox::result_t setup(setup_context* ctx);
+}
+#endif //OMNIX_SETUP_H
